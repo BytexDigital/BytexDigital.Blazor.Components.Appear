16918ff4fec310f71e2f6710d485d8e09f02697f
Ryan Torzynski <ryantorzynski@gmail.com>
2020-01-02 17:08:06 +0100

    Projektdateien hinzufügen.

---
+++ BytexDigital.Blazor.Components.Appear.Core/Extensions/HtmlHelperAppearExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Html;
+
+namespace Microsoft.AspNetCore.Mvc.Rendering
+{
+    public static class HtmlHelperAppearExtensions
+    {
+        public static HtmlString IncludeAppearScripts(this IHtmlHelper htmlHelper)
+        {
+            return new HtmlString($"<script src='/_content/BytexDigital.Blazor.Components.Appear/bundle.js'></script>");
+        }
+    }
+}
